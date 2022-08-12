4733f75b176dd53093e2b1f6a318390c5308ff2a
paoner <paoner@gmail.com>
2022-08-12 15:31:36 +0800

    font change

--- app/components/modals/AboutModal/AboutModal.js
+++ app/components/modals/AboutModal/AboutModal.js
@@ -21,21 +21,21 @@ class AboutModal extends React.Component {
             <T id="aboutModal.paragraph2a" m="Decrediton is free and open source software, developed and designed by the global team of"/> {/*<a onClick={() => shell.openExternal("https://decred.org/contributors/")}>*/}{/*<T id="aboutModal.paragraph2b" m="Decred contributors"/></a>*/}
           </div>
           <div className="about-modal-text-paragraph">
-            <T id="aboutModal.paragraph3" m="Want to help or get involved, check out"/> <a onClick={() => shell.openExternal("https://github.com/Decred-Next/mydcrn")}>github.com/Decred-Next/mydcrn</a>
+            <T id="aboutModal.paragraph3" m="Want to help or get involved, check out"/> <a onClick={() => shell.openExternal("https://github.com/dcrnofficial/mydcrn")}>github.com/dcrnofficial/mydcrn</a>
           </div>
         </div>
         <div className="about-modal-bottom-area">
           <div className="about-modal-bottom-area-left">
             <T id="aboutModal.version" m="Version"/>
             -{version}
-            {/*<a className="about-modal-upgrade" onClick={() => shell.openExternal("https://github.com/Decred-Next/binary-release/release/tag/v")}></a>
+            {/*<a className="about-modal-upgrade" onClick={() => shell.openExternal("https://github.com/dcrnofficial/binary-release/release/tag/v")}></a>
             {version} -&nbsp;
             {updateAvailable ?
             <a className="about-modal-upgrade" onClick={() => shell.openExternal("https://github.com/decred/decred-binaries/releases")}><T id="aboutModal.upgradeAvailable" m="Upgrade Available"/></a> :
             <a className="about-modal-upgrade" onClick={() => shell.openExternal("https://github.com/decred/decred-binaries/releases/tag/v"+`${version}`)}><T id="aboutModal.whatsNew" m="What's New?"/></a>*/}
           </div>
           <div className="about-modal-bottom-area-middle">
-            Copyright &copy; 2022 <a onClick={() => shell.openExternal("https://dcrn.xyz")}>Decred-Next</a>
+            Copyright &copy; 2022 <a onClick={() => shell.openExternal("https://dcrn.xyz")}>DCRN</a>
           </div>
           {/*<div className="about-modal-bottom-area-right">
             <a onClick={() => shell.openExternal("https://github.com/decred/decrediton/blob/master/LICENSE")}><T id="aboutModal.licensing" m="Licensing information"/></a>
